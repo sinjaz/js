6a8190330ccaafc7eeddd8693b9b930aba11613d
Sina <sin.jaz@gmail.com>
2017-02-18 21:26:57 -0500

    Constructor pattern

--- inheritance-(Constructor-Pattern)/index.js
+++ inheritance-(Constructor-Pattern)/index.js
@@ -1,11 +1,18 @@
 log = console.log;
 
 function Person(name, age) {
-    return {
-        name: name,
-        age: age
-    }
+    this.name = name;
+    this.age = age;
+}
+
+Person.prototype.greet = function () {
+    log(this.name + ' is ' + this.age + ' years old.');
 }
 
 var sina = new Person('Sina', 12);
-log(sina);
+sina.greet();
+var ben = new Person('Ben', 9);
+ben.greet();
+
+log(sina instanceof Person); // true
+log(sina === ben); // false
