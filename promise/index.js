f64f2917d24e6e5cf6e5c0ab5e1e872360d7428c
Sina Jazayeri <sin.jaz@gmail.com>
2017-08-16 21:56:10 -0400

    Promises

--- promise/index.js
+++ promise/index.js
@@ -1,12 +1,15 @@
-function getData() {
-
+function getData(input) {
+  console.log('input: ' + input);
   return new Promise(function(resolve, reject){
       setTimeout(function(){
-          resolve('some data');
+        resolve('output: ' + input);
       }, 2000);
   });
 }
 
-getData().then(function(data){
-  console.log('data:', data);
+getData(1).then(function(data){
+  console.log('data: ', data);
+  return getData(2);
+}).then(function(data){
+  console.log('data: ', data);
 });
