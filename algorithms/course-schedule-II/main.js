9a9d8ea73759a6cc9cf45d1ba814cada92ba15b4
Sina Jazayeri <sina@colorcode.io>
2020-02-26 20:43:40 -0500

    algo

--- algorithms/course-schedule-II/main.js
+++ algorithms/course-schedule-II/main.js
@@ -6,6 +6,14 @@
  * @return {number[]}
  */
 var findOrder = function (numCourses, prerequisites) {
+  // Loop through prerequisits
+  // Create a hash map <COURSE>, mapping { course: prerequisits }
+  // Create a hash map <PREREQ>, mapping { prerquist: courses }
+  // Find courses without prereqs by
+    // looping from 0 to n-1 and checking each index in the hash map and seeing if there is an array. If undefined, there are no prereqs
+  // For each course without a prereq, add it to the beginning of the list of the output
+  // Loop over each course in the output array and append its dependant to the output array and keep going until no more items in the output array
+  
   let prereqs = {};
   let courses = {};
   for (let i = 0; i < prerequisites.length; i++) {
@@ -61,11 +69,3 @@ var findOrder = function (numCourses, prerequisites) {
 // findOrder(2, [[1, 0]]);
 // findOrder(1, []);
 findOrder(3, [[0, 1], [0, 2], [1, 2]]);
-
-// Loop through prerequisits
-  // Create a hash map <COURSE>, mapping { course: prerequisits }
-  // Create a hash map <PREREQ>, mapping { prerquist: courses }
-  // Find courses without prereqs by
-    // looping from 0 to n-1 and checking each index in the hash map and seeing if there is an array. If undefined, there are no prereqs
-  // For each course without a prereq, add it to the beginning of the list of the output
-  // Loop over each course in the output array and append its dependant to the output array and keep going until no more items in the output array
--- algorithms/generate-parentheses/main.js
+++ algorithms/generate-parentheses/main.js
@@ -1,30 +1,35 @@
+// https://leetcode.com/problems/generate-parentheses/
+
 /**
  * @param {number} n
  * @return {string[]}
  */
 var generateParenthesis = function(n) {
+  // Create an output array to push all the possibilities into
+  // Create a function (addParen) that will take a prebuilt partial string and the number of opening and closing parenthesis left to use
+    // If there are any opening parenthesis left, add one to the string and call the function recursively
+    // If there are any closing parenthesis left, as long as the number of closing parenthesis is higher than opening ones, add one to the string and call the function recursively.
+    // If there are no more parenthesis left, it must mean the string has used all 6 parenthesis, so add it to the output array
+  // Return the output array
 
-  let all = [];
-
-  function build(all, string = '', left, right) {
-    if (left === 0 && right === 0) {
-      all.push(string);
+  let output = [];
+  function addParent(string, opening, closing) {
+    if (opening > 0) {
+      addParent(string + '(', opening - 1, closing);
     }
 
-    if (left > 0) {
-      build(all, string + "(", left - 1, right);
+    if (closing > 0 && closing > opening) {
+      addParent(string + ')', opening, closing - 1);
     }
 
-    if (right > 0 && right > left) {
-      build(all, string + ")", left, right - 1);
+    if (opening === 0 && closing === 0) {
+      output.push(string);
     }
   }
-  build(all, '', n, n);
 
-  return all;
+  addParent('', n, n);
+
+  return output;
 };
 
-console.log(generateParenthesis(3));
-
-
-
+console.log(generateParenthesis(2));
